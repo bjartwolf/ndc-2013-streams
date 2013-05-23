e22d58521cf4158f0a73da0dd27d1a379adf8b8c
bjartwolf <bjartnes@gmail.com>
2013-05-23 12:06:54 +0200

    refactoring

--- facedetectionFromVideoFile.js
+++ facedetectionFromVideoFile.js
@@ -1,43 +1,46 @@
-var rx = require('rx');
+// STREAMS part
 var fs = require('fs');
-var events= require('events');
-events.EventEmitter.prototype.toObservable = require('./toObservable.js');
-
 var RGBA = require('./RGBAStream');
 var FrameStream = require('./FrameStream');
+var frame = new FrameStream();
 var FaceStream = require('./FaceStream');
+var face = new FaceStream();
+var Serializer = require('./serializer'); 
+
 //var ffplay = require('./ffplay'); // can use this instead of mplayer
 var mplayer = require('./mplayer');
-var frame = new FrameStream();
-var face = new FaceStream();
-
 var fileStream = fs.createReadStream('drone.mp4');
 
 fileStream.pipe(mplayer.stdin);
 fileStream.pipe(RGBA.stdin);
 
 RGBA.stdout.pipe(frame).pipe(face);
+//face.pipe(new Serializer()).pipe(process.stdout)
 
-var faceStream = face.toObservable('data')
+// RX part
+var rx = require('rx');
+var events= require('events');
+events.EventEmitter.prototype.toObservable = require('./toObservable.js');
+
+rx.Observable.prototype.pipe = require('./writeToStream').writeToStream;
+
+var obsFaces = face.toObservable('data')
     .select(function(faces) {
         return faces[0];
     });
 
-faceStream
-    .where(function (face) {
-            return !face || face.confidence <= -1; 
-    })
-     .subscribe(function () {
-        console.log("NO FACE DETECTED");
-    });
+var faceFound = function (face) {
+    return (face && face.confidence > 1);
+};
 
-faceStream
-    .where(function (face) {
-        if (face) { return face.confidence > -1; }   
-    })
+obsFaces
+    .where(faceFound)
     .select(function (face) {
-        return "Face at " + face.x + ", " + face.y + ". Width is " + face.width;
+        return 'Face at ' + face.x + ', ' + face.y + '. Width is ' + face.width + '\n';
     })
-     .subscribe(function (face) {
-        console.log(face);
-    });
+    .pipe(process.stdout)
+
+obsFaces
+    .where(function (face) { return !faceFound(face);})
+    .select(function () { return 'Nothing detected...\n';})
+    .pipe(process.stdout);
