07a2003d4377c00daa8d6c99d8b9bd207b66d1de
bjartwolf <bjartnes@gmail.com>
2013-05-30 20:08:54 +0200

    updating eiw integration

--- facedetectionNet.js
+++ facedetectionNet.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var player = require('./mplayer'); //var player = require('./ffplay'); 
 var videoStream = fs.createReadStream('drone.mp4');
 //var videoStream = require('./droneVideoStream');
 
