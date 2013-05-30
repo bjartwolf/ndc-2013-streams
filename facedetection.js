6caadf3f98f16f61a0a86f4260d00262fbdd5da8
bjartwolf <bjartnes@gmail.com>
2013-05-30 09:24:46 +0200

    removed mplayer

--- facedetection.js
+++ facedetection.js
@@ -1,9 +1,9 @@
 // STREAMS part
 // *******
 var fs = require('fs');
-var player = require('./mplayer'); //var player = require('./ffplay'); 
-var videoStream = fs.createReadStream('drone.mp4');
-//var videoStream = require('./droneVideoStream');
+var player = require('./ffplay'); 
+//var videoStream = fs.createReadStream('drone.mp4');
+var videoStream = require('./droneVideoStream');
 
 videoStream.pipe(player.stdin);
 
