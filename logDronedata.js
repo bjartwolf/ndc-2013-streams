3d3759e6a368471cf9b444eb1c7d23a03960ac67
bjartwolf <bjartnes@gmail.com>
2013-05-30 08:55:40 +0200

    set drone to go forward

--- logDronedata.js
+++ logDronedata.js
@@ -19,10 +19,14 @@ drone.takeoff();
 drone
     .after(5000, function () {
         this.clockwise(0.5);
-        })
+    })
     .after(1000, function () {
         this.up(0.1);
-        })
+    })
+    .after(1000, function () {
+        this.front(0.1);
+    })
     .after(5000, function () {
+        this.stop();
         this.land();
-        });
+    });
