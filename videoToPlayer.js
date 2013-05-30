5a78c820b39772789fe9beb4536f9ba614f47d82
bjartwolf <bjartnes@gmail.com>
2013-05-30 09:07:03 +0200

    removed mplayer

--- videoToPlayer.js
+++ videoToPlayer.js
@@ -1,7 +1,7 @@
 // Part One
 // *********
 var fs = require('fs');
-var player = require('./mplayer'); //var player = require('./ffplay'); 
+var player = require('./ffplay'); 
 var videoStream = fs.createReadStream('drone.mp4');
 //var videoStream = require('./droneVideoStream');
 
