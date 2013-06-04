6a0a632a93826bb63d4ed18b09053a4e881a74f8
bjartwolf <bjartnes@gmail.com>
2013-06-04 19:55:07 +0200

    added the http video stream

--- videoToPlayer.js
+++ videoToPlayer.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 
-var videoStream = fs.createReadStream('ytdl.mp4');
-
+//var videoStream = fs.createReadStream('ytdl.mp4');
 //var videoStream = require('./droneVideoStream'); 
+var videoStream = require('./httpMovieStream'); 
+
 var player = require('./ffplay'); 
 videoStream.pipe(player.stdin);
 
