fc5e72a75bbccd257b5d0ca67a844b30f58779f2
bjartwolf <bjartnes@gmail.com>
2013-06-04 12:29:29 +0200

    pulled video to main

--- ffplay.js
+++ ffplay.js
@@ -1,10 +1,8 @@
 var spawn = require('child_process').spawn;
 var ffplay = spawn('ffplay', [
-//    '-f', 'h264',
-//    '-analyzeduration', '0',
+    '-analyzeduration', '0',
     '-autoexit',
     '-'
-//    '-i', '-'
   ], {stdio: ['pipe', 'ignore', 'ignore']}
 );
 
