01f3822dc1b2062f07bf1213e97f3a580261b93f
bjartwolf <bjartnes@gmail.com>
2013-06-04 20:05:37 +0200

    removed mplayer as it won't be used

--- mplayer.js
+++
@@ -1,6 +0,0 @@
-var spawn = require('child_process').spawn;
-//var mplayer = spawn('mplayer', ['-'], {stdio: [null,1,2]}); // logs
-
-var mplayer = spawn('mplayer', ['-'])
-mplayer.on('error', function (err) { console.log(err);});
-module.exports = mplayer;
