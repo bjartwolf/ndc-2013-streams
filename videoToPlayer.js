0bfe154ad6f9d1b292b4eeb68c18f6f1c02a3304
bjartwolf <bjartnes@gmail.com>
2013-06-04 09:04:45 +0200

    ok

--- videoToPlayer.js
+++ videoToPlayer.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
+
 var videoStream = fs.createReadStream('ytdl.mp4');
+
 //var videoStream = require('./droneVideoStream'); 
 var player = require('./ffplay'); 
 videoStream.pipe(player.stdin);
+
